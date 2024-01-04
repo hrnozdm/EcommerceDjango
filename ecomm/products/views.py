5ada10da00d30ad8c155e4d572210358b2f6ca8b
Harun Özdemir <80276787+hrnozdm@users.noreply.github.com>
2024-01-04 17:22:42 +0300

    product Update

--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
+
+
+
+def get_product(request,slug):
+    try:
+       product = Product.objects.get(slug=slug)
+       return render(request,'product/product.html',context={'product':product})
+    except Exception as e:
+        print(e)
+    
+
 
 
